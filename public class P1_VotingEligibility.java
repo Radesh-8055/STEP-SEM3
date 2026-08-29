8029173710f2a668c611186d415963c524f0869e
S.Radesh <radesh7780558019@gmail.com>
2026-08-29 13:12:11 +0530

    Add files via upload

---
+++ public class P1_VotingEligibility.java
@@ -0,0 +1,17 @@
+public class P1_VotingEligibility {
+
+    public static void checkVotingEligibility(int age) {
+        boolean eligible = age >= 18;
+
+        if (eligible) {
+            System.out.println("Eligible to vote");
+        } else {
+            System.out.println("Not eligible to vote");
+        }
+    }
+
+    public static void main(String[] args) {
+        checkVotingEligibility(20);
+        checkVotingEligibility(16);
+    }
+}
